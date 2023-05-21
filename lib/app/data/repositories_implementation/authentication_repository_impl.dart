6db190a35e0f92602697c82cb45c360a7a6de34b
Bryan Zapata <bmzc95@gmail.com>
2023-05-21 13:18:00 -0500

    functional programming

--- lib/app/data/repositories_implementation/authentication_repository_impl.dart
+++ lib/app/data/repositories_implementation/authentication_repository_impl.dart
@@ -1,12 +1,26 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
 import '../../domain/models/user.dart';
 import '../../domain/repositories/authentication_repository.dart';
 
+const _key = 'sessionId';
+
 class AuthenticationRepositoryImpl implements AuthenticationRepository {
+  final FlutterSecureStorage _flutterSecureStorage;
+
+  AuthenticationRepositoryImpl(
+    this._flutterSecureStorage,
+  );
+
   @override
   Future<User?> getUserData() {
     return Future.value(null);
   }
 
   @override
-  Future<bool> get isSignedIn => Future.value(true);
+  Future<bool> get isSignedIn async {
+    final sessionId = await _flutterSecureStorage.read(key: _key);
+
+    return sessionId != null;
+  }
 }
