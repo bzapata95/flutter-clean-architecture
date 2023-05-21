ae0717f0eba5d9a1d1bd66234d2d4bf2e534d4e7
Bryan Zapata <bmzc95@gmail.com>
2023-05-20 20:20:28 -0500

    structure clean architecture

---
+++ android/app/src/main/kotlin/com/zapatadev/tv/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.zapatadev.tv
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:tv/app/my_app.dart';
+
+main() {
+  runApp(const MyApp());
+}
